434a708c2703977d24e7b72a0723b6b658f756f7
MZigomar <vincent_robart@hotmail.fr>
2021-12-06 17:26:48 +0100

    fin

--- workspace/JS/TP2/assets/js/script.js
+++ workspace/JS/TP2/assets/js/script.js
@@ -1,22 +1,25 @@
 const htmlSections = document.getElementsByTagName("section");
 const sections = [].slice.call(htmlSections);
 
+// Get all "li"
 const controller = document.getElementById("controller").childNodes;
 const lis = [];
 controller.forEach(e => {
     e.tagName=="LI" ? lis.push(e) : console.log(); ;
 });
 
+// add d-none class
 const hide = () => sections.forEach(e => {
     e.classList.add("d-none");
-
 });
+// remove select class
 const unselect = () => {
     lis.forEach(e => {
         e.classList.remove("select")
     });
 }
 
+// add onclick event on all 
 lis.forEach(e => {
     e.onclick = () => {
         hide();
