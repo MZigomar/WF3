b3cecdb998991e562b9a9be9927727f8281ca6e0
MZigomar <vincent_robart@hotmail.fr>
2021-12-06 09:50:39 +0100

    JS de la semaine dernière

--- workspace/JS/TP1/assets/js/script.js
+++ workspace/JS/TP1/assets/js/script.js
@@ -29,34 +29,64 @@ const taskItems = [
     },
 ];
 
+const filterList = document.getElementById("filterList");
+const filter = document.getElementById("filterTask");
+const addTask = document.getElementById("addTask");
+const cancel = document.getElementById("addPopup__form__cancel");
+const submit = document.getElementById("addPopup__form__submit");
+const addPopup = document.getElementById("addPopup");
+const filterPopup = document.getElementById("filterPopup");
+const taskList = document.getElementById("taskList");
+const cat = document.getElementById("filterSelect");
+const text = document.getElementById("filterText");
 
-document.getElementById("addTask").onclick = function () {
-    console.log("Plus button was clicked")
-    document.getElementById("addPopup").classList.remove("hidden");
+
+
+const changeVisibility = (id) => id.classList.contains("hidden") ? id.classList.remove("hidden") : id.classList.add("hidden");
+
+filter.onclick = () => changeVisibility(filterPopup);
+
+addTask.onclick = () => changeVisibility(addPopup);
+
+cancel.onclick = (e) => {
+    changeVisibility(addPopup);
+    e.preventDefault();
 };
-document.getElementById("addPopup__form__cancel").onclick = function () {
-    console.log("Cancel was clicked")
-    document.getElementById("addPopup").classList.add("hidden");
-};
 
-document.getElementById("filterTask").onclick = function () {
-    console.log("Filter was clicked")
-    let popup = document.getElementById("filterPopup");
-    popup.classList.contains("hidden") ? popup.classList.remove("hidden") : popup.classList.add("hidden");
-};
-
-
-function remove (i) {
-    taskItems.splice(i, 1);
-    displayTask();
+submit.onclick = (e) => {
+    changeVisibility(addPopup);
+    e.preventDefault();
+    add(cat.value, text.value);
+    document.getElementById("filterText").value = "";
+    document.getElementById("filterSelect").value = 0;
 }
 
-for (let i = 0; i < taskItems.length; i++) {
-    document.getElementById("taskList").innerHTML +=
-        `<div class="task">
-            <p class="task__category">${taskItems[i].category}</p>
-            <p class="task__text">${taskItems[i].text}</p>
-            <p class="task__delete" onclick="remove(${i})">X</p>
-        </div>`;
-};
+function update() {
+    console.log("updated");
+    taskList.innerHTML = "";
+    for (let i = 0; i < taskItems.length; i++) {
+        taskList.innerHTML +=
+            `<div class="task">
+                <p class="task__category">${taskItems[i].category}</p>
+                <p class="task__text">${taskItems[i].text}</p>
+                <p class="task__deconste" onclick="remove(${i})">X</p>
+            </div>`;
+    };
+}
 
+const add = (category, text) => {
+    if (text != "") {
+        taskItems.push({ category, text })
+        update();
+    }
+}
+
+const remove = (i) => {
+    taskItems.splice(i, 1);
+    update();
+}
+
+for (let i = 0; i < 4; i++)
+    cat.innerHTML += `<option value="${i}">${i}</option>`
+
+update();
